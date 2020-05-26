ace693457946316d74c71eaa298cf32fcadd35b7
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-05-26 22:15:27 +0900

    add change_chrname_[to|from]_Greek.pl

---
+++ scripts/change_chrname_from_Greek.pl
@@ -0,0 +1,40 @@
+#! /usr/bin/perl -w
+
+open(IN, $ARGV[0]) || die;
+while(<IN>) {
+    next if($_ eq "\n");
+    chomp;
+
+    my @clm = split(/\t/, $_);
+    my $head = "";
+    my $chr = $clm[0];
+    if($chr =~ /chr(.+)/) {
+      $head="chr";
+      $chr = $1;
+    }
+
+    if($chr eq "I"){ $chr = "1"; }
+    elsif($chr eq "II"){ $chr = "2"; }
+    elsif($chr eq "III"){ $chr = "3"; }
+    elsif($chr eq "IV"){ $chr = "4"; }
+    elsif($chr eq "V"){ $chr = "5"; }
+    elsif($chr eq "VI"){ $chr = "6"; }
+    elsif($chr eq "VII"){ $chr = "7"; }
+    elsif($chr eq "VIII"){ $chr = "8"; }
+    elsif($chr eq "IX"){ $chr = "9"; }
+    elsif($chr eq "X"){ $chr = "10"; }
+    elsif($chr eq "XI"){ $chr = "11"; }
+    elsif($chr eq "XII"){ $chr = "12"; }
+    elsif($chr eq "XIII"){ $chr = "13"; }
+    elsif($chr eq "XIV"){ $chr = "14"; }
+    elsif($chr eq "XV"){ $chr = "15"; }
+    elsif($chr eq "XVI"){ $chr = "16"; }
+
+    print "$head";
+    for($i=0;$i<=$#clm;$i++) {
+	print "$clm[$i]";
+	if($i!=$#clm) {print"\t";}
+	else {print"\n";}
+    }
+}
+close IN;
---
+++ scripts/change_chrname_to_Greek.pl
@@ -0,0 +1,40 @@
+#! /usr/bin/perl -w
+
+open(IN, $ARGV[0]) || die;
+while(<IN>) {
+    next if($_ eq "\n");
+    chomp;
+
+    my @clm = split(/\t/, $_);
+    my $head = "";
+    my $chr = $clm[0];
+    if($chr =~ /chr(.+)/) {
+      $head="chr";
+      $chr = $1;
+    }
+
+    if($chr eq "1"){ $chr = "I"; }
+    elsif($chr eq "2"){ $chr = "II"; }
+    elsif($chr eq "3"){ $chr = "III"; }
+    elsif($chr eq "4"){ $chr = "IV"; }
+    elsif($chr eq "5"){ $chr = "V"; }
+    elsif($chr eq "6"){ $chr = "VI"; }
+    elsif($chr eq "7"){ $chr = "VII"; }
+    elsif($chr eq "8"){ $chr = "VIII"; }
+    elsif($chr eq "9"){ $chr = "IX"; }
+    elsif($chr eq "10"){ $chr = "X"; }
+    elsif($chr eq "11"){ $chr = "XI"; }
+    elsif($chr eq "12"){ $chr = "XII"; }
+    elsif($chr eq "13"){ $chr = "XIII"; }
+    elsif($chr eq "14"){ $chr = "XIV"; }
+    elsif($chr eq "15"){ $chr = "XV"; }
+    elsif($chr eq "16"){ $chr = "XVI"; }
+
+    print "$head";
+    for($i=0;$i<=$#clm;$i++) {
+	print "$clm[$i]";
+	if($i!=$#clm) {print"\t";}
+	else {print"\n";}
+    }
+}
+close IN;
