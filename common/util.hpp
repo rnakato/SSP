44c1acead76b1a8c1b50912144680189d5510687
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2021-06-15 22:28:13 +0900

    remove GaussianSmoothing

--- common/util.hpp
+++ common/util.hpp
@@ -42,44 +42,6 @@ int32_t getmaxi(std::vector<T> v)
   return maxi;
 };
 
-/*template <class T>
-void GaussianSmoothing(std::vector<T> &v, const int32_t nsmooth)
-{
-  std::vector<double> w(nsmooth+1,0);
-  double var(1);
-
-  double sum(0);
-  for (int32_t i=0; i<=nsmooth; ++i) {
-    w[i] = exp(static_cast<double>(-i*i)/2*var*var);
-    sum += w[i];
-  }
-  double r(1/(sum*2 - w[0]));
-
-  std::vector<double> m(nsmooth+1,0);
-  for (int32_t i=0; i<=nsmooth; ++i) m[i] = v[nsmooth-i];
-
-  //  std::cout << "GS Weight: ";
-  //for (int32_t i=0; i<=nsmooth; ++i)  std::cout << w[i] << "\t";
-  //std::cout  << "r: " << r << std::endl;
-
-  for (size_t i=nsmooth; i<v.size()-nsmooth; ++i) {
-    m[0] = v[i];
-
-    //    std::cout << "before: ";
-    //    for (int32_t j=0; j<nsmooth; ++j) std::cout << v[i-nsmooth+j] << "\t";
-    // for (int32_t j=nsmooth; j>=0; --j) std::cout << m[j] << "\t";
-    //for (int32_t j=1; j<=nsmooth; ++j) std::cout << v[i+j] << "\t";
-    //std::cout << std::endl;
-    double val(w[0]*m[0]);
-    for (int32_t j=1; j<=nsmooth; ++j) val += w[j] * (m[j] + v[i+j]);
-    v[i] = val*r;
-    //std::cout << v[i] << std::endl;
-
-    for (int32_t i=nsmooth; i>0; --i) m[i] = m[i-1];
-  }
-  return;
-}*/
-
 template <class T>
 int32_t findIndex(std::vector<T> array, T value)
 {
