d2716b2334111b6cd0e0de7944ab61c2288de3b5
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-04-09 18:11:16 +0900

    version 1.2.3

--- src/SeqStats.hpp
+++ src/SeqStats.hpp
@@ -115,12 +115,10 @@ public:
     int32_t d;
     for (auto strand: {Strand::FWD, Strand::REV}) {
       if(strand == Strand::FWD) d = flen; else d = -flen;
-      if(d<=0) PRINTERR_AND_EXIT("fragment length <= 0.");
 
       for(auto &x: seq[strand].vRead) {
 	x.F5 = std::max(x.F3 + d, 0);
 	x.F5 = std::min(x.F5, (int32_t)(getlen()-1));
-	//if (x.F5 > getlen()-1) x.F5 = getlen()-1;
       }
     }
   }
